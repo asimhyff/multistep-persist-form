7f1859ee6bdf8301ab16ec2133ced447d8d241b5
asimhyff <asimhyff@users.noreply.github.com>
2020-02-05 16:38:06 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,6 @@ const Home = () => (
 
     <Nav />
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
 
       <div className="row">
         <a href="https://nextjs.org/docs" className="card">
